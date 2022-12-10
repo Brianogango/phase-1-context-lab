1393ffd964ce0b3c86fe494a63a22579cd1b7441
brian <brian.ogango@student.moringaschool.com>
2022-12-10 17:48:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 /* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+//          createEmployeeRecords
+function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map(array => createEmployeeRecord(array))
+}
+//          createTimeInEvent
+function createTimeInEvent(employeeRecord, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return employeeRecord
+}
+//          createTimeOutEvent
+function createTimeOutEvent(employeeRecord, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return employeeRecord
+}
+//          hoursWorkedOnDate
+function hoursWorkedOnDate(employeeRecord, date){
+    let timeIn = employeeRecord.timeInEvents.find(timeIn => timeIn.date === date)
+    let timeOut = employeeRecord.timeOutEvents.find(timeOut => timeOut.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+//          wagesEarnedOnDate
+function wagesEarnedOnDate(employeeRecord, date){
+    return hoursWorkedOnDate(employeeRecord, date) * employeeRecord.payPerHour
+}
+//          allWagesFor
+function allWagesFor(employeeRecord){
+    let dates = employeeRecord.timeInEvents.map(timeIn => timeIn.date)
+    let wages = dates.map(date => wagesEarnedOnDate(employeeRecord, date))
+    return wages.reduce((total, wage) => total + wage)
+}
+//          findEmployeeByFirstName
+function calculatePayroll(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce((total, employeeRecord) => total + allWagesFor(employeeRecord), 0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
